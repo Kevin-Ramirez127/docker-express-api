f288923e681d3d5c7da322c71d8f81b174647bd9
Kevin-Ramirez127 <kevinsilvaram@gmail.com>
2024-04-19 17:09:36 -0300

    CONFIG: sequelize package

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:20.9.0-alpine
 WORKDIR /myapp
 COPY package.json .
 RUN npm install
+RUN npm install -g nodemon
 
 COPY . .
 CMD npm start
--- src/app.js
+++ src/app.js
@@ -3,19 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-let { createPool } = require('mysql2/promise');
 require('dotenv/config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const pool = createPool({
-  user: 'root',
-  host: process.env.MYSQL_HOST,
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  port: process.env.MYSQL_DOCKER_PORT,
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,13 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
-app.use('/ping', async (req, res) => {
-  const result = await pool.query('SELECT NOW()');
-
-  res.send(result[0]);
-
-});
 
 app.use('/users', usersRouter);
 
